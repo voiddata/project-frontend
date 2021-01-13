f871ea5ab0a1d5208ff6e8b432b1495200b0232e
voiddata <sasidhar6998@gmail.com>
2021-01-13 12:15:31 +0530

    seperated admin login and user login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -34,31 +34,4 @@ export class LoginComponent implements OnInit {
      })
     }
 }
-/*
-export class LoginComponent {
-
-  login: Login = new Login();
-  message: string;
-  
-  constructor(private adminService: AdminService, private router: Router) { }
-
-  loginCheck() {
-    console.log(this.login);
-    this.adminService.login(this.login).subscribe(response => {
-      alert(JSON.stringify(response));
-      console.log(response);
-      if(response.status == 'SUCCESS') {
-        let Id = response.Id;
-        let userName = response.userName;
-        sessionStorage.setItem('Id', String(Id));
-        sessionStorage.setItem('userName', userName);
-        this.router.navigate(['dashboard']);
-      }
-      else
-        this.message = response.message;
-    })
-  }
-
-}*/
-
 
